819466d116c731980516e0871a750f5f14f2c60f
Tiarintsoa <rihantiana000@gmail.com>
2026-02-25 15:26:31 +0300

    [feature] API URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://m1p13mean-tiarintsoa-jason-backend.onrender.com/'
+  apiUrl: 'https://m1p13mean-tiarintsoa-jason-backend.onrender.com/api'
 };
